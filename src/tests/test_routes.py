09022ccdc9d8a4df99c8d7f9a74f9b0b288a3c63
Vlad <nazipovvt@yandex.ru>
2023-01-11 02:52:13 +0500

    Сделал тесты

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -1,10 +1,45 @@
 import pytest
 
+from src.calculations.vlp import calc_vlp
+from src.models.models import VlpCalcResponse
+
 
 @pytest.fixture
 def generate_data_for_tests():
-    pass
+    return {
+        "inclinometry": {
+            "MD": [0, 1000, 1500],
+            "TVD": [0, 1000, 1100]
+        },
+        "casing": {
+            "d": 0.1
+            },
+        "tubing": {
+            "d": 0.062,
+            "h_mes": 1000
+        },
+        "pvt": {
+            "wct": 50,
+            "rp": 100,
+            "gamma_oil": 0.8,
+            "gamma_gas": 0.7,
+            "gamma_wat": 1,
+            "t_res": 90
+        },
+        "p_wh": 10,
+        "geo_grad": 3,
+        "h_res": 1500
+    }
 
 
 def test_calc_model_success(api_client, generate_data_for_tests):
-    pass
+    expected_result = VlpCalcResponse.parse_obj(
+        calc_vlp(**generate_data_for_tests)
+    ).dict()
+
+    actual_result = api_client.post(
+        "http://localhost:8001/vlp/calc",
+        json=generate_data_for_tests
+    ).text
+
+    assert eval(actual_result) == expected_result
